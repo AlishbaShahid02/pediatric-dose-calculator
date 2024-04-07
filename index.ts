0df0308e1a45f7a6aa9124e635e12f5667726bee
AlishbaShahid02 <163669338+AlishbaShahid02@users.noreply.github.com>
2024-04-07 22:55:38 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,13 @@
+#! /usr/bin/env node
+
+import inquirer from 'inquirer';
+import chalk from 'chalk';
+
+console.log(chalk.cyan.bold(`"Welcome to prediatric dose calculator"`));
+
+let input = await inquirer.prompt([
+{name: "weight", type: "number", message: "Enter child's weight in pounds"},
+{name: "dose", type: "number", message: "Enter adult dose"},
+]);
+let prediatric_dose = input.weight / 150 * input.dose;
+console.log(chalk.magentaBright.italic(`Prediatric dose is: ${prediatric_dose}`));
