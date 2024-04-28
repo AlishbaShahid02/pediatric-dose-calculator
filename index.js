6a05160099b4ac1423392fe27aa861c7f87e6b6b
AlishbaShahid02 <163669338+AlishbaShahid02@users.noreply.github.com>
2024-04-28 19:25:05 +0500

    changes in dose

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import chalk from 'chalk';
 console.log(chalk.cyan.bold(`"Welcome to prediatric dose calculator"`));
 let input = await inquirer.prompt([
     { name: "weight", type: "number", message: "Enter child's weight in pounds" },
-    { name: "dose", type: "number", message: "Enter adult dose" },
+    { name: "dose", type: "number", message: "Enter adult dose in mg" },
 ]);
 let prediatric_dose = input.weight / 150 * input.dose;
-console.log(chalk.magentaBright(`Prediatric dose is: ${prediatric_dose}`));
+console.log(chalk.magentaBright.italic(`Prediatric dose is: ${prediatric_dose}`));
